3314040468eb3a109dc546acbeb9c43d0b445a2e
RodolpheThienard <thienard.rodolphe1@gmail.com>
2023-12-27 13:08:53 +0100

    Berendsen Thermostat Not finished yet

--- src/kernels.c
+++ src/kernels.c
@@ -125,3 +125,28 @@ cinetic (particules_t p, f64 m, size_t n)
   return cinetic;
 }
 
+/* TODO
+   cinétic moment
+f64 *
+startup_dim (f64 *dim)
+{
+  for (u32 i = 0; i < SIZE; i++)
+    {
+      f64 c = (f64)rand_r (&i);
+      f64 s = (f64)rand_r (&i);
+      dim[i] = ;
+    }
+}
+
+void
+startup ()
+{
+}
+*/
+
+/* Compute Temperature at m_step */
+void
+berendsen_thermostat (particules_t p, particules_t default_p, size_t m_step)
+{
+  // T = (default_p.x * GAMMA * T0) / p.x + 1;
+}
